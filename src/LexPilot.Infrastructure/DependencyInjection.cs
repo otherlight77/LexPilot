5242bb26fe8505a4810a797fb6dd69c3ad44d613
otherlight77 <pirezjeremy@gmail.com>
2026-07-05 07:30:08 +0200

    Add infrastructure dependency injection

---
+++ src/LexPilot.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using LexPilot.Infrastructure.Data;
+using LexPilot.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LexPilot.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services
+            .AddIdentityCore<ApplicationUser>()
+            .AddRoles<IdentityRole<Guid>>()
+            .AddEntityFrameworkStores<AppDbContext>();
+
+        return services;
+    }
+}
